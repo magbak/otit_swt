06f5ee0301016a65c9104c9369b74eb472f4f952
Magnus Bakken <magbak@gmail.com>
2022-04-21 10:26:47 +0200

    Videre

--- parser/src/ast.rs
+++ parser/src/ast.rs
@@ -5,73 +5,116 @@ pub enum ConnectiveType {
     SEMICOLON,
     DASH,
     SLASH,
-    BACKSLASH
+    BACKSLASH,
 }
 
 impl ConnectiveType {
-    pub fn new(ctype:&char) -> ConnectiveType {
+    pub fn new(ctype: &char) -> ConnectiveType {
         match ctype {
-            ':' => {ConnectiveType::COLON}
-            '.' => {ConnectiveType::PERIOD}
-            ';' => {ConnectiveType::SEMICOLON}
-            '-' => {ConnectiveType::DASH}
-            '/' => {ConnectiveType::SLASH}
-            '\\' => {ConnectiveType::BACKSLASH}
-            _ => {panic!("Should only be called with valid connective type")}
+            ':' => ConnectiveType::COLON,
+            '.' => ConnectiveType::PERIOD,
+            ';' => ConnectiveType::SEMICOLON,
+            '-' => ConnectiveType::DASH,
+            '/' => ConnectiveType::SLASH,
+            '\\' => ConnectiveType::BACKSLASH,
+            _ => {
+                panic!("Should only be called with valid connective type")
+            }
         }
     }
 }
 
 #[derive(PartialEq, Debug)]
 pub struct Connective {
-    connective_type:ConnectiveType,
-    number_of:usize
+    pub(crate) connective_type: ConnectiveType,
+    pub(crate) number_of: usize,
 }
 
 impl Connective {
-    pub fn new(ctype:&char, number_of:usize) -> Connective {
-        Connective{ connective_type: ConnectiveType::new(ctype), number_of }
+    pub fn new(ctype: &char, number_of: usize) -> Connective {
+        Connective {
+            connective_type: ConnectiveType::new(ctype),
+            number_of,
+        }
     }
 }
 
 #[derive(PartialEq, Debug)]
-pub struct PathElementOrConnective {
-    path_element:Option<PathElement>,
-    connective:Option<Connective>
-}
-
-impl PathElementOrConnective {
-    pub fn from_path_element(p:PathElement) -> PathElementOrConnective{
-        PathElementOrConnective{ path_element: Some(p), connective: None }
-    }
-    pub fn from_connective(c:Connective) -> PathElementOrConnective {
-        PathElementOrConnective{ path_element: None, connective: Some(c) }
-    }
+pub enum PathElementOrConnective {
+    PathElement(PathElement),
+    Connective(Connective)
 }
 
 #[derive(PartialEq, Debug)]
 pub struct PathElement {
-    element:String
+    element: String,
 }
 
 impl PathElement {
-    pub fn new(element:&str) -> PathElement{
-        PathElement{element:element.to_string()}
+    pub fn new(element: &str) -> PathElement {
+        PathElement {
+            element: element.to_string(),
+        }
     }
 }
 
 #[derive(PartialEq, Debug)]
 pub struct Path {
-    path:Vec<PathElementOrConnective>
+    path: Vec<PathElementOrConnective>,
 }
 
 impl Path {
-    pub fn new(p:Vec<PathElementOrConnective>) -> Path {
-        Path{path:p}
+    pub fn new(p: Vec<PathElementOrConnective>) -> Path {
+        Path { path: p }
     }
 
-    pub fn prepend(&mut self, pe:PathElementOrConnective) {
+    pub fn prepend(&mut self, pe: PathElementOrConnective) {
         self.path.insert(0, pe);
     }
 }
 
+#[derive(PartialEq, Debug)]
+pub enum BooleanOperator {
+    NEQ,
+    EQ,
+    LTEQ,
+    GTEQ,
+    LT,
+    GT,
+    LIKE,
+}
+
+impl BooleanOperator {
+    pub fn new(o: &str) -> BooleanOperator {
+        match o {
+            "!=" => BooleanOperator::NEQ,
+            "=" => BooleanOperator::EQ,
+            "<=" => BooleanOperator::LTEQ,
+            ">=" => BooleanOperator::GTEQ,
+            "<" => BooleanOperator::LT,
+            ">" => BooleanOperator::GT,
+            "LIKE" => BooleanOperator::LIKE,
+            _ => {
+                panic!("Unknown operator {:}", o)
+            }
+        }
+    }
+}
+
+pub enum Literal {
+    REAL(f64),
+    INTEGER(i32),
+    STRING(String)
+}
+
+pub enum PathOrLiteral {
+    PATH(Path),
+    LITERAL(Literal)
+}
+
+#[derive(PartialEq, Debug)]
+pub struct ConditionedPath {
+    lhs_path:Path,
+    boolean_operator: BooleanOperator,
+    rhs_path_or_literal: PathOrLiteral
+}
--- parser/src/myparser.rs
+++ parser/src/myparser.rs
@@ -1,13 +1,13 @@
 extern crate nom;
 
-use crate::ast::{Connective, Path, PathElement, PathElementOrConnective};
+use crate::ast::{BooleanOperator, ConditionedPath, Connective, ConnectiveType, Literal, Path, PathElement, PathElementOrConnective, PathOrLiteral};
 use nom::branch::alt;
 use nom::bytes::complete::{tag, take_while, take_while1};
 use nom::character::complete::{alpha1, char};
 use nom::character::is_alphabetic;
 use nom::combinator::{complete, recognize};
 use nom::multi::many1;
-use nom::sequence::tuple;
+use nom::sequence::{pair, tuple};
 use nom::IResult;
 
 fn connective(c: &str) -> IResult<&str, Connective> {
@@ -17,8 +17,9 @@ fn connective(c: &str) -> IResult<&str, Connective> {
         many1(char(':')),
         many1(char(';')),
         many1(char('/')),
-        many1(char('\\'))))(c)?;
-   assert!(conns.len() > 0);
+        many1(char('\\')),
+    ))(c)?;
+    assert!(conns.len() > 0);
     Ok((c, Connective::new(conns.get(0).unwrap(), conns.len())))
 }
 
@@ -28,18 +29,15 @@ fn path_element(p: &str) -> IResult<&str, PathElement> {
 }
 
 fn singleton_path(p: &str) -> IResult<&str, Path> {
-    let el = path_element(p);
-    match el {
-        Ok((_, pe)) => Ok((
-            p,
-            Path::new(vec![PathElementOrConnective::from_path_element(pe)]),
-        )),
-        Err(e) => Err(e),
-    }
+    let (p, el) = path_element(p)?;
+    Ok((
+        p,
+        Path::new(vec![PathElementOrConnective::from_path_element(el)]),
+    ))
 }
 
 fn path_triple(p: &str) -> IResult<&str, Path> {
-    let (p,(pe, conn, mut pa)) = tuple((path_element, connective, path))(p)?;
+    let (p, (pe, conn, mut pa)) = tuple((path_element, connective, path))(p)?;
     let conn_or = PathElementOrConnective::from_connective(conn);
     let pe_or = PathElementOrConnective::from_path_element(pe);
     pa.prepend(conn_or);
@@ -51,8 +49,55 @@ fn path(p: &str) -> IResult<&str, Path> {
     alt((path_triple, singleton_path))(p)
 }
 
+fn path_or_literal(pl: &str) -> IResult<&str, PathOrLiteral> {
+
+}
+
+fn conditionedPath(cp: &str) -> IResult<&str, ConditionedPath> {
+    let (cp,(p, bop, pol) ) = tuple((path, boolean_operator, path_or_literal))(cp);
+}
+
+fn numeric_literal(l: &str) -> IResult<&str, Literal> {}
+
+fn string_literal(s: &str) -> IResult<&str, Literal> {}
+
+fn literal(l: &str) -> IResult<&str, Literal> {}
+
+fn boolean_operator(o: &str) -> IResult<&str, BooleanOperator> {
+    let (o, opstr) = alt((
+            tag("="),
+            tag("!="),
+            tag(">"),
+            tag("<"),
+            tag(">="),
+            tag("<="),
+            tag("like")
+        ))(o)?;
+    Ok((o, BooleanOperator::new(opstr)))
+}
+
+fn condition(c: &str) -> IResult<&str, Condition> {
+    let (c, cond) = pair(
+        ws(boolean_operator),
+        alt((literal, path))
+    )(c);
+}
+
 #[test]
 fn test_parse_path() {
     assert_eq!(connective("-"), Ok(("", Connective::new(&'-', 1))));
-    assert_eq!(path_triple("Abc.cda"), Ok(("", Path::new(vec![]))));
+    assert_eq!(
+        path("Abc.cda"),
+        Ok((
+            "",
+            Path::new(vec![
+                PathElementOrConnective::from_path_element(PathElement::new("Abc")),
+                PathElementOrConnective::from_connective(Connective {
+                    connective_type: ConnectiveType::PERIOD,
+                    number_of: 1
+                }),
+                PathElementOrConnective::from_path_element(PathElement::new("cda"))
+            ])
+        ))
+    );
 }
