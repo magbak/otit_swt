586c4d316655777f90421e317646ede3ab6d645a
Magnus Bakken <magbak@gmail.com>
2022-06-27 20:11:54 +0200

    Fix performance

--- mapper/src/lib.rs
+++ mapper/src/lib.rs
@@ -1,4 +1,9 @@
 #[macro_use] extern crate unic_char_range;
 
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 pub mod parser;
 pub mod ast;
--- mapper/tests/test_parser.rs
+++ mapper/tests/test_parser.rs
@@ -1,7 +1,7 @@
 use mapper::ast::{
-    Argument, ConstantLiteral, ConstantTerm, Directive, Instance, Parameter, Prefix, PrefixedName,
-    ResolvesToNamedNode, Signature, Statement, StottrDocument, StottrTerm, StottrVariable,
-    Template,
+    Argument, ConstantLiteral, ConstantTerm, Directive, Instance, PType, Parameter, Prefix,
+    PrefixedName, ResolvesToNamedNode, Signature, Statement, StottrDocument, StottrTerm,
+    StottrVariable, Template,
 };
 use mapper::parser::stottr_doc;
 use nom::Finish;
@@ -9,6 +9,8 @@ use oxrdf::{BlankNode, NamedNode};
 
 #[test]
 fn test_easy_template() {
+    //This test case is taken from:
+    // https://github.com/Callidon/pyOTTR/blob/master/tests/stottr_test.py
     let stottr = r#"
         @prefix ex: <http://example.org#>.
         ex:Person[ ?firstName, ?lastName, ?email ] :: {
@@ -84,6 +86,184 @@ fn test_easy_template() {
                     },
                 ],
             }],
+        })],
+    };
+    assert_eq!(expected, doc);
+}
+
+#[test]
+fn test_spec_modifiers_1() {
+    let stottr = r#"
+        @prefix ex:<http://example.net/ns#>.
+
+    ex:NamedPizza [ ??pizza  ] .
+    "#;
+
+    let (s, doc) = stottr_doc(stottr).finish().expect("Ok");
+    assert_eq!("", s);
+    let expected = StottrDocument {
+        directives: vec![Directive::Prefix(Prefix {
+            name: "ex".to_string(),
+            iri: NamedNode::new_unchecked("http://example.net/ns#"),
+        })],
+        statements: vec![Statement::Signature(Signature {
+            template_name: ResolvesToNamedNode::PrefixedName(PrefixedName {
+                prefix: "ex".to_string(),
+                name: "NamedPizza".to_string(),
+            }),
+            parameter_list: vec![Parameter {
+                optional: true,
+                non_blank: false,
+                ptype: None,
+                stottr_variable: StottrVariable {
+                    name: "pizza".to_string(),
+                },
+                default_value: None,
+            }],
+            annotation_list: None,
+        })],
+    };
+    assert_eq!(expected, doc);
+}
+
+#[test]
+fn test_spec_modifiers_2() {
+    let stottr = r#"
+        @prefix ex:<http://example.net/ns#>.
+
+    ex:NamedPizza [ !?pizza  ] .
+    "#;
+
+    let (s, doc) = stottr_doc(stottr).finish().expect("Ok");
+    assert_eq!("", s);
+    let expected = StottrDocument {
+        directives: vec![Directive::Prefix(Prefix {
+            name: "ex".to_string(),
+            iri: NamedNode::new_unchecked("http://example.net/ns#"),
+        })],
+        statements: vec![Statement::Signature(Signature {
+            template_name: ResolvesToNamedNode::PrefixedName(PrefixedName {
+                prefix: "ex".to_string(),
+                name: "NamedPizza".to_string(),
+            }),
+            parameter_list: vec![Parameter {
+                optional: false,
+                non_blank: true,
+                ptype: None,
+                stottr_variable: StottrVariable {
+                    name: "pizza".to_string(),
+                },
+                default_value: None,
+            }],
+            annotation_list: None,
+        })],
+    };
+    assert_eq!(expected, doc);
+}
+
+#[test]
+fn test_spec_modifiers_3() {
+    let stottr = r#"
+        @prefix ex:<http://example.net/ns#>.
+
+    ex:NamedPizza [ ?!?pizza  ] .
+    "#;
+
+    let (s, doc) = stottr_doc(stottr).finish().expect("Ok");
+    assert_eq!("", s);
+    let expected = StottrDocument {
+        directives: vec![Directive::Prefix(Prefix {
+            name: "ex".to_string(),
+            iri: NamedNode::new_unchecked("http://example.net/ns#"),
+        })],
+        statements: vec![Statement::Signature(Signature {
+            template_name: ResolvesToNamedNode::PrefixedName(PrefixedName {
+                prefix: "ex".to_string(),
+                name: "NamedPizza".to_string(),
+            }),
+            parameter_list: vec![Parameter {
+                optional: true,
+                non_blank: true,
+                ptype: None,
+                stottr_variable: StottrVariable {
+                    name: "pizza".to_string(),
+                },
+                default_value: None,
+            }],
+            annotation_list: None,
+        })],
+    };
+    assert_eq!(expected, doc);
+}
+
+#[test]
+fn test_spec_modifiers_4() {
+    let stottr = r#"
+        @prefix ex:<http://example.net/ns#>.
+
+    ex:NamedPizza [ !??pizza  ] .
+    "#;
+
+    let (s, doc) = stottr_doc(stottr).finish().expect("Ok");
+    assert_eq!("", s);
+    let expected = StottrDocument {
+        directives: vec![Directive::Prefix(Prefix {
+            name: "ex".to_string(),
+            iri: NamedNode::new_unchecked("http://example.net/ns#"),
+        })],
+        statements: vec![Statement::Signature(Signature {
+            template_name: ResolvesToNamedNode::PrefixedName(PrefixedName {
+                prefix: "ex".to_string(),
+                name: "NamedPizza".to_string(),
+            }),
+            parameter_list: vec![Parameter {
+                optional: true,
+                non_blank: true,
+                ptype: None,
+                stottr_variable: StottrVariable {
+                    name: "pizza".to_string(),
+                },
+                default_value: None,
+            }],
+            annotation_list: None,
+        })],
+    };
+    assert_eq!(expected, doc);
+}
+
+#[test]
+fn test_spec_type_1() {
+    let stottr = r#"
+        @prefix ex:<http://example.net/ns#>.
+
+    ex:NamedPizza [ owl:Class ?pizza ].
+    "#;
+
+    let (s, doc) = stottr_doc(stottr).finish().expect("Ok");
+    assert_eq!("", s);
+    let expected = StottrDocument {
+        directives: vec![Directive::Prefix(Prefix {
+            name: "ex".to_string(),
+            iri: NamedNode::new_unchecked("http://example.net/ns#"),
+        })],
+        statements: vec![Statement::Signature(Signature {
+            template_name: ResolvesToNamedNode::PrefixedName(PrefixedName {
+                prefix: "ex".to_string(),
+                name: "NamedPizza".to_string(),
+            }),
+            parameter_list: vec![Parameter {
+                optional: false,
+                non_blank: false,
+                ptype: Some(PType::BasicType(PrefixedName {
+                    prefix: "owl".to_string(),
+                    name: "Class".to_string(),
+                })),
+                stottr_variable: StottrVariable {
+                    name: "pizza".to_string(),
+                },
+                default_value: None,
+            }],
+            annotation_list: None,
         })],
     };
     assert_eq!(expected, doc);
