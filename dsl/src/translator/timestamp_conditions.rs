95aa60aaed395ea80a517391f743d1a71c7c1e34
Magnus Bakken <magbak@gmail.com>
2022-07-08 07:09:50 +0200

    Restructure translator

---
+++ dsl/src/translator/timestamp_conditions.rs
@@ -0,0 +1,38 @@
+use oxrdf::{Literal, Variable};
+use oxrdf::vocab::xsd;
+use spargebra::algebra::Expression;
+use crate::ast::TsQuery;
+use crate::costants::{TIMESTAMP_VARIABLE_NAME, XSD_DATETIME_FORMAT};
+use super::Translator;
+
+impl Translator {
+    pub fn add_timestamp_conditions(&mut self, ts_query: &TsQuery) {
+        let mut timestamp_conditions = vec![];
+        if let Some(from_ts) = ts_query.from_datetime {
+            let gteq = Expression::GreaterOrEqual(
+                Box::new(Expression::Literal(Literal::new_typed_literal(
+                    format!("{}", from_ts.format(XSD_DATETIME_FORMAT)),
+                    xsd::DATE_TIME,
+                ))),
+                Box::new(Expression::Variable(Variable::new_unchecked(
+                    TIMESTAMP_VARIABLE_NAME,
+                ))),
+            );
+            timestamp_conditions.push(gteq);
+        }
+
+        if let Some(to_ts) = ts_query.to_datetime {
+            let gteq = Expression::LessOrEqual(
+                Box::new(Expression::Literal(Literal::new_typed_literal(
+                    format!("{}", to_ts.format(XSD_DATETIME_FORMAT)),
+                    xsd::DATE_TIME,
+                ))),
+                Box::new(Expression::Variable(Variable::new_unchecked(
+                    TIMESTAMP_VARIABLE_NAME,
+                ))),
+            );
+            timestamp_conditions.push(gteq);
+        }
+        self.conditions.append(&mut timestamp_conditions);
+    }
+}
