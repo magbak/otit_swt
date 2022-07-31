a55d651ee9e1cb055a31110c290a31e4c4df422a
Magnus Bakken <magbak@gmail.com>
2022-07-31 22:46:59 +0200

    Fix small error

--- py-query/tests/test_basics.py
+++ py-query/tests/test_basics.py
@@ -43,7 +43,7 @@ def test_simple_query(dremio_testdata, oxigraph_testdata):
     pl.testing.assert_frame_equal(df, expected_df, check_dtype=False)
 
 
-def test_dsl_query():#(dremio_testdata, oxigraph_testdata):
+def test_dsl_query(dremio_testdata, oxigraph_testdata):
     engine = Engine(OXIGRAPH_QUERY_ENDPOINT)
     tables = [
         TimeSeriesTable(
